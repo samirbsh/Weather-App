9e8426413bf6424f4f8e790a90b1f2f4d077d576
samirbsh <samirkumar2527@gmail.com>
2020-09-12 22:54:29 +0530

    code improvement with object destructuring

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ if (process.argv[2] === undefined) {
   console.log("No location was provided as input.");
 } else {
   //Callback Chaining
-  geocode(address, (error, data) => {
+  geocode(address, (error, {latitude, longitude, location}={}) => {     // a default object is provided
     // we will have access here after geocode function has completed
     if (error) {
       return console.log(error);
     }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return console.log(error);
       }
-      console.log(data.location);
+      console.log(location);
       console.log("Data", forecastData);
     });
   });
--- utils/forecast.js
+++ utils/forecast.js
@@ -5,26 +5,27 @@ const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=${encodeURIComponent(
     latitude
   )},${encodeURIComponent(longitude)}`;
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather service !!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find Weather !!", undefined);
     } else {
       callback(
-        undefined,'Condition is '+
-        response.body.current.weather_descriptions[0] +
+        undefined,
+        "Condition is " +
+          body.current.weather_descriptions[0] +
           ". And it is " +
-          response.body.current.temperature +
+          body.current.temperature +
           " " +
           "and feels like " +
-          response.body.current.feelslike +
+          body.current.feelslike +
           " in " +
-          response.body.location.name +
+          body.location.name +
           ", " +
-          response.body.location.region +
+          body.location.region +
           " ," +
-          response.body.location.country
+          body.location.country
       );
     }
   });
