606b285cf51236c034ba008ac0594808e9f6d062
samirbsh <samirkumar2527@gmail.com>
2020-09-12 18:24:05 +0530

    chaining callback function

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const geocode = require("./utils/geocode.js");
 const forecast = require("./utils/forecast.js");
 
-geocode("Patna", (error, data) => {
-  // we will have access here after geocode function has completed
-  console.log("Error : ", error);
-  console.log("data : ", data);
-});
-
-forecast(25.61, 85.14139, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+const address = process.argv[2];
+if (process.argv[2] === undefined) {
+  console.log("No location was provided as input.");
+} else {
+  //Callback Chaining
+  geocode(address, (error, data) => {
+    // we will have access here after geocode function has completed
+    if (error) {
+      return console.log(error);
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log("Data", forecastData);
+    });
+  });
+}
--- utils/forecast.js
+++ utils/forecast.js
@@ -5,24 +5,27 @@ const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=${encodeURIComponent(
     latitude
   )},${encodeURIComponent(longitude)}`;
-  console.log(url);
   request({ url: url, json: true }, (error, response) => {
     if (error) {
       callback("Unable to connect to weather service !!", undefined);
     } else if (response.body.error) {
       callback("Unable to find Weather !!", undefined);
     } else {
-      callback(undefined, {
-        weatherDescription: response.body.current.weather_descriptions[0],
-        temperature: response.body.current.temperature,
-        feelsLike: response.body.current.feelslike,
-        location:
+      callback(
+        undefined,'Condition is '+
+        response.body.current.weather_descriptions[0] +
+          ". And it is " +
+          response.body.current.temperature +
+          " " +
+          "and feels like " +
+          response.body.current.feelslike +
+          " in " +
           response.body.location.name +
           ", " +
           response.body.location.region +
           " ," +
-          response.body.location.country,
-      });
+          response.body.location.country
+      );
     }
   });
 };
