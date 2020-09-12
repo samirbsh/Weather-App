7259cca99450b509afbf22bbe5ff518ae7bb1317
samirbsh <samirkumar2527@gmail.com>
2020-09-12 11:40:52 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const request = require("postman-request");
-/*
+
 const url =
   "http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=37.8267,-122.4233&units=f";
 request({ url: url,json:true}, (error, response) => {
@@ -13,7 +13,7 @@ request({ url: url,json:true}, (error, response) => {
         console.log(response.body.current.weather_descriptions[0]+ '. It is currently '+response.body.current.temperature+" degrees out . It feels like "+ response.body.current.feelslike+" degrees out.");
     }  
 });
-*/
+
 // Geocoding : Process of taking an address and converting them into latitude and longitude pair 
 // Adress -> Lat/Long -> Weather
 const geoCodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2FtaXJrcjI1MjciLCJhIjoiY2tleG9tZmRuNGZuazJ4cGkzbzZsM3V5ciJ9.d3aXbSrMRBcGyznIWJiKyA&limit=1";
