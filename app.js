036e1a6f623417f9081756de9234181e98838ca8
samirbsh <samirkumar2527@gmail.com>
2020-09-12 15:31:10 +0530

    forecast callback function added

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-const request = require("postman-request");
 const geocode = require("./utils/geocode.js")
-// const url =
-//   "http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=37.8267,-122.4233&units=f";
-// request({ url: url,json:true}, (error, response) => {
-//     if (error) {
-//         console.log("Unable to connect to weather service !!");
-//     }
-//     else if(response.body.error){
-//         console.log("Unable to find Weather !!")
-//     }
-//      else {
-//         console.log(response.body.current.weather_descriptions[0]+ '. It is currently '+response.body.current.temperature+" degrees out . It feels like "+ response.body.current.feelslike+" degrees out.");
-//     }  
-// });
+const forecast = require("./utils/forecast.js")
 
 geocode('Patna',(error,data)=>{
     // we will have access here after geocode function has completed
     console.log('Error : ',error)
     console.log('data : ',data)
 })
+
+forecast(25.61, 85.14139, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,27 +1,26 @@
 const request = require("postman-request");
-// Geocoding : Process of taking an address and converting them into latitude and longitude pair 
+// Geocoding : Process of taking an address and converting them into latitude and longitude pair
 // Adress -> Lat/Long -> Weather
 const geocode = (address, callback) => {
-    const geoCodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoic2FtaXJrcjI1MjciLCJhIjoiY2tleG9tZmRuNGZuazJ4cGkzbzZsM3V5ciJ9.d3aXbSrMRBcGyznIWJiKyA&limit=1`;
-    request({ url:geoCodeURL,json:true}, (error,response) => {
+  const geoCodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=pk.eyJ1Ijoic2FtaXJrcjI1MjciLCJhIjoiY2tleG9tZmRuNGZuazJ4cGkzbzZsM3V5ciJ9.d3aXbSrMRBcGyznIWJiKyA&limit=1`;
+  request({ url: geoCodeURL, json: true }, (error, response) => {
     if (error) {
-        callback('Unable to connect to the Location Service !!',undefined);
+      callback("Unable to connect to the Location Service !!", undefined);
+    } else if (response.body.message) {
+      callback("Unable to find Location !!", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Invalid Search !! Try another search.", undefined);
+    } else {
+      const coordinates = response.body.features[0].center;
+      callback(undefined, {
+        latitude: coordinates[1],
+        longitude: coordinates[0],
+        location: response.body.features[0].place_name,
+      });
     }
-    else if(response.body.message){
-        callback('Unable to find Location !!',undefined)
-    } 
-    else if(response.body.features.length === 0){
-        callback("Invalid Search !! Try another search.",undefined)
-    }
-    else {
-        const coordinates = response.body.features[0].center; 
-        callback(undefined,{
-            latitude: coordinates[1],
-            longitude: coordinates[0],
-            location: response.body.features[0].place_name
-        })
-    }
-})
-}
+  });
+};
 
-module.exports = geocode
+module.exports = geocode;
