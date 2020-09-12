a0575ec9020407b2788e18a504cb9b9003d97c01
samirbsh <samirkumar2527@gmail.com>
2020-09-12 15:32:20 +0530

    formatted

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const geocode = require("./utils/geocode.js")
-const forecast = require("./utils/forecast.js")
+const geocode = require("./utils/geocode.js");
+const forecast = require("./utils/forecast.js");
 
-geocode('Patna',(error,data)=>{
-    // we will have access here after geocode function has completed
-    console.log('Error : ',error)
-    console.log('data : ',data)
-})
+geocode("Patna", (error, data) => {
+  // we will have access here after geocode function has completed
+  console.log("Error : ", error);
+  console.log("data : ", data);
+});
 
 forecast(25.61, 85.14139, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+  console.log("Error", error);
+  console.log("Data", data);
+});
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,9 +1,11 @@
 const request = require("postman-request");
 
 const forecast = (latitude, longitude, callback) => {
-    //const url = "http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=37.8267,-122.4233"
-    const url = `http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}`;
-    console.log(url)
+  //const url = "http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=37.8267,-122.4233"
+  const url = `http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=${encodeURIComponent(
+    latitude
+  )},${encodeURIComponent(longitude)}`;
+  console.log(url);
   request({ url: url, json: true }, (error, response) => {
     if (error) {
       callback("Unable to connect to weather service !!", undefined);
@@ -14,7 +16,12 @@ const forecast = (latitude, longitude, callback) => {
         weatherDescription: response.body.current.weather_descriptions[0],
         temperature: response.body.current.temperature,
         feelsLike: response.body.current.feelslike,
-        location: response.body.location.name+ ', '+response.body.location.region+' ,'+response.body.location.country
+        location:
+          response.body.location.name +
+          ", " +
+          response.body.location.region +
+          " ," +
+          response.body.location.country,
       });
     }
   });
