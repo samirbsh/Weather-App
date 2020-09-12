de2425a39670eb5ee6eede10659600422ff2bf60
samirbsh <samirkumar2527@gmail.com>
2020-09-12 14:47:29 +0530

    Callback Abstraction

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
 const request = require("postman-request");
+const geocode = require("./utils/geocode.js")
+// const url =
+//   "http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=37.8267,-122.4233&units=f";
+// request({ url: url,json:true}, (error, response) => {
+//     if (error) {
+//         console.log("Unable to connect to weather service !!");
+//     }
+//     else if(response.body.error){
+//         console.log("Unable to find Weather !!")
+//     }
+//      else {
+//         console.log(response.body.current.weather_descriptions[0]+ '. It is currently '+response.body.current.temperature+" degrees out . It feels like "+ response.body.current.feelslike+" degrees out.");
+//     }  
+// });
 
-const url =
-  "http://api.weatherstack.com/current?access_key=c86b952bd99f8e889997f77aaf4d27fe&%20query=37.8267,-122.4233&units=f";
-request({ url: url,json:true}, (error, response) => {
-    if (error) {
-        console.log("Unable to connect to weather service !!");
-    }
-    else if(response.body.error){
-        console.log("Unable to find Weather !!")
-    }
-     else {
-        console.log(response.body.current.weather_descriptions[0]+ '. It is currently '+response.body.current.temperature+" degrees out . It feels like "+ response.body.current.feelslike+" degrees out.");
-    }  
-});
-
-// Geocoding : Process of taking an address and converting them into latitude and longitude pair 
-// Adress -> Lat/Long -> Weather
-const geoCodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2FtaXJrcjI1MjciLCJhIjoiY2tleG9tZmRuNGZuazJ4cGkzbzZsM3V5ciJ9.d3aXbSrMRBcGyznIWJiKyA&limit=1";
-// const geoCodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${"Delhi"}.json?access_token=pk.eyJ1Ijoic2FtaXJrcjI1MjciLCJhIjoiY2tleG9tZmRuNGZuazJ4cGkzbzZsM3V5ciJ9.d3aXbSrMRBcGyznIWJiKyA&limit=1`;
-request({ url:geoCodeURL,json:true}, (error,response) => {
-    if (error) {
-        console.log("Unable to connect to the Location Service !!");
-    }
-    else if(response.body.message){
-        console.log("Unable to find Location !!")
-    } 
-    else if(response.body.features.length === 0){
-        console.log("Invalid Search !! Try another search.")
-    }
-    else {
-        const coordinates = response.body.features[0].center;
-    latitude = coordinates[1]
-    longitude = coordinates[0]
-    console.log("latitude = " + latitude+" longitude = " + longitude)
-    }
+geocode('Patna',(error,data)=>{
+    // we will have access here after geocode function has completed
+    console.log('Error : ',error)
+    console.log('data : ',data)
 })
